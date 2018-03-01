ea120a56969a6f797c38d3d2c5a5f52edf0d1f67
Harel Hasky <harelnh@campus.technion.ac.il>
2018-03-01 06:30:10 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <my-component></my-component>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+  var Child = {
+    template: '<div>A custom component!</div>'
+  }
+
+  export default {
+  name: 'app',
+    components: {
+    'my-component':Child,
+    }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
